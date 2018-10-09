c824e828f2621af0ca6eeb01e1f2466bc37f05db
LimJiHun <ljh11077@naver.com>
2018-10-09 14:56:44 +0900

    월요일 학원 끝날때로 돌아감
    푸쉬 푸쉬

---
+++ MoDacProject/src/com/modu/modac/service/PartnerService.java
@@ -0,0 +1,26 @@
+package com.modu.modac.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PartnerService {
+	
+	
+	
+	//로그인 용
+	boolean isMember(Map map);
+	//목록용]
+	List<PartnerDto> selectList(Map map);
+	//상세보기용]
+	PartnerDto selectOne(Map map);
+	//입력/수정/삭제용]
+	int delete(Map map);
+	int insert(Map map);
+
+	int updateone(Map map);
+	int updatetwo(Map map);
+	
+	//전체 레코드 수]
+	int getTotalRecord(Map map);
+
+}
---
+++ MoDacProject/src/com/modu/modac/service/impl/PartnerServiceimpl.java
@@ -0,0 +1,77 @@
+package com.modu.modac.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.modu.modac.service.PartnerService;
+import org.springframework.stereotype.Service;
+import com.modu.modac.service.PartnerService;
+import com.modu.modac.service.PartnerDto;
+
+@Service("partnerService")
+public class PartnerServiceimpl implements PartnerService {
+	
+	@Resource(name="partnerDao")
+	private PartnerDao partnerdao;
+
+	
+	@Override
+	public boolean isMember(Map map) {
+		
+		return partnerdao.isMember(map);
+	}
+
+	@Override
+	public List<PartnerDto> selectList(Map map) {
+		
+		return partnerdao.selectList(map);
+	}
+
+	@Override
+	public int getTotalRecord(Map map) {
+		
+		return partnerdao.getTotalRecord(map);
+	}
+
+	@Override
+	public PartnerDto selectOne(Map map) {
+		
+		return partnerdao.selectOne(map);
+	}
+
+	@Override
+	public int delete(Map map) {
+		
+		return partnerdao.delete(map);
+	}
+
+	@Override
+	public int insert(Map map) {					
+		return partnerdao.insert(map);
+	}
+
+
+	@Override
+	public int updateone(Map map) {
+		
+		return partnerdao.updateone(map);
+	}
+
+	@Override
+	public int updatetwo(Map map) {
+	
+		return partnerdao.updatetwo(map);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}//PartnerServiceimpl
+
